7ef464b53e882d2c76fcec71e9b67121e1fce921
Gentlezheng <905717436@qq.com>
2021-07-16 10:40:08 +0800

    Update testANN.py

--- testANN.py
+++ testANN.py
@@ -16,18 +16,18 @@ def start(file_name):
     except Exception:
         movement_matrix, init_position_matrix = Gm.get_position_X_(file_name)
     # movement_matrix, init_position_matrix = Gm.get_position(file_name)
-    # 控制器初始化
+    # Controller initialization
     node_num = init_position_matrix.shape[0]
     print('节点数：{}\n'.format(node_num))
     controller = Init.init_controller(node_num, 'G')
     controller.feature_junction_matrix_construction(node_num)
     global RG_feature
     RG_feature = controller.feature_junction_matrix
-    # 位置数据处理
-    init_position_arranged = init_position_matrix[np.lexsort(init_position_matrix[:, ::-1].T)]  # 按第一列顺序排序
+    # Location data processing
+    init_position_arranged = init_position_matrix[np.lexsort(init_position_matrix[:, ::-1].T)]  # Sort by first column order
     node_position = init_position_arranged[0]
 
-    # 节点初始化 各个节点的初始位置和节点编号
+    # Node initialization The initial position and node number of each node
     node_list = (Init.init_node(node_position, controller))
 
     effi = 0
@@ -37,49 +37,49 @@ def start(file_name):
     pt_e = 0
     pt_sum = 0
     rec_delay = 0
-    # 生成通信节点
+    # Generate communication node
     round = 10
 
     for i in range(round):
         com_node_list = []
         com_node_list.extend(Init.get_communication_node(node_num - 1))
         print('通信节点列表：\n', com_node_list)
-        ## 统计各个车辆在当前秒中的负载
+        ## Count the load of each vehicle in the current second
         # v_load = [0 for i in range(len(node_list))]
         start_time = t.time()
-        # 以秒为间隔进行
+        # In seconds
         for time in range(200, sim_time, 1):
             print('\nTime: %d' % time)
-            # 处理位置矩阵
+            # Processing position matrix
             current_move = movement_matrix[np.nonzero(movement_matrix[:, 0].A == time)[0], :]
 
             for line in current_move:
                 for i in range(1, 4):
                     node_position[int(line[0, 1]), i] = line[0, i + 1]
             node_id_position = node_position[:, [1, 2, 3]]  # 当前current_move 的后三项特征[x,y,z]
-            # 所有节点更新位置，并发送hello至控制器
+            # All nodes update their positions and send hello to the controller
             for node in node_list:
                 node.update_node_position(node_id_position)
                 node.generate_hello(controller)
 
-            # 控制器更新网络全局情况
+            # The controller updates the global situation of the network
             controller.predict_position()
             controller.junction_matrix_construction(node_num)
             pt_s = t.time()
             ANN.DataGet(controller, node_list, time)
             pt_e = t.time()
             pt_sum += pt_e - pt_s
-            # 通信节点生成数据包并发送请求至控制器
+            # The communication node generates a data packet and sends a request to the controller
             node_list[com_node_list[time % int((node_num * Gp.com_node_rate) / 2 - 1)][0]].generate_request(
                 com_node_list[time % int((node_num * Gp.com_node_rate) / 2 - 1)][1], controller, 1024)
-            # 控制器处理路由请求
+            # The controller handles the routing request
             print('\nrouting request')
             controller.resolve_request(node_list, 1)
-            # 所有节点处理错误路由修复请求
+            # All nodes process error routing repair requests
             print('\nerror request')
             controller.resolve_error(node_list, 1)
             print('\nforward')
-            # 所有节点开始转发分组
+            # All nodes start to forward packets
             for node in node_list:
                 node.forward_pkt_to_nbr(node_list, controller)
             jh.delete()
